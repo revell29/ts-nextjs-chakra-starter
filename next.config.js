4ae13208f1e29f212f1d06808b9fe441390b18cc
apsya Dira <diraapsya0@gmail.com>
2022-01-30 22:32:26 +0700

    chore: initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['src'],
+  },
 
-module.exports = nextConfig
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
